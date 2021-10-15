5c123cdf7cb3d2342545b0172ce23312ba0bfbd7
james (phuong.doand) <ext-phuong.doand@spdigital.sg>
2021-10-15 17:35:00 +0800

    Init Project

---
+++ ticketing/ticketing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ticketing
+//
+//  Created by Doan Duy Phuong on 15/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
